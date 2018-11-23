8893217c8913aea3d36cc283ad36daf9eb59e01e
nniikkoollaa <nniikkoollaa@gmail.com>
2018-11-23 10:20:51 +0100

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,3 +1,3 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx2
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "Hello World from updated host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
